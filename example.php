be87e629e7346f1178e64cc9cef5c07d1d725135
levsion <levsion@163.com>
2019-08-05 14:16:57 +0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,27 @@
+//配置log常量
+if(IS_ONLINE_SERVER)
+{
+    //define('NEW_LOG_HOST','');
+    define('NEW_LOG_PORT','5151');
+    define('NEW_LOG_CHANNEL','mapi');
+    define('NEW_LOG_EXECUTE_TIME',0.1);
+    define('NEW_LOG_REQUEST',false);
+    define('NEW_LOG_REQUEST_PARAM',false);
+}else if(IS_DEV_SERVER)
+{
+    //define('NEW_LOG_HOST','');
+    define('NEW_LOG_PORT','5152');
+    define('NEW_LOG_CHANNEL','mapi');
+    define('NEW_LOG_EXECUTE_TIME',0);
+    define('NEW_LOG_REQUEST',true);
+    define('NEW_LOG_REQUEST_PARAM',true);
+}else{
+    //define('NEW_LOG_HOST','');
+    define('NEW_LOG_PORT','5153');
+    define('NEW_LOG_CHANNEL','mapi');
+    define('NEW_LOG_EXECUTE_TIME',0);
+    define('NEW_LOG_REQUEST',true);
+    define('NEW_LOG_REQUEST_PARAM',true);
+}
+
+new_log::log("hello new_log",'log_key');
